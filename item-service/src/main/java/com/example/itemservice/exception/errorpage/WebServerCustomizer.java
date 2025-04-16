cc382f63e57c42dd2ba1b91b7613aef8cfc3e592
User <dlguswls456@naver.com>
2025-04-16 16:45:15 +0900

    springboot exception

--- item-service/src/main/java/com/example/itemservice/exception/errorpage/WebServerCustomizer.java
+++ item-service/src/main/java/com/example/itemservice/exception/errorpage/WebServerCustomizer.java
@@ -4,13 +4,12 @@ import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 
 
 /**
  * 서블릿 오류 페이지 등록
  */
-@Component
+//@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
     @Override
