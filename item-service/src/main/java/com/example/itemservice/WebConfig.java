ac6f20527d2f0035acbb5215ae8a74640b83a47d
User <dlguswls456@naver.com>
2025-04-18 10:56:17 +0900

    file upload with servlet

--- item-service/src/main/java/com/example/itemservice/WebConfig.java
+++ item-service/src/main/java/com/example/itemservice/WebConfig.java
@@ -60,7 +60,7 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addInterceptor(new LoginCheckInterceptor()).order(2).addPathPatterns("/**").excludePathPatterns("/",
                                         "/members/add", "/login", "/logout", "/css/**", "/*.ico", "/error",
-                                        "/exception/**", "/converter/**");
+                                        "/exception/**", "/converter/**", "/upload/**");
     }
 
     @Override
