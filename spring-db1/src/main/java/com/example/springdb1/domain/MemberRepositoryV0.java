c6c5e8ea35033197661b24e0d31641f809bd6591
unknown <dlguswls456@naver.com>
2025-04-18 18:06:09 +0900

    jdbc with h2db crud

--- spring-db1/src/main/java/com/example/springdb1/domain/MemberRepositoryV0.java
+++ spring-db1/src/main/java/com/example/springdb1/domain/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.NoSuchElementException;
 
 import com.example.springdb1.DBConnectionUtil;
 
@@ -17,6 +18,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MemberRepositoryV0 {
 
+    /**
+     * 회원 저장
+     * 
+     * @param member
+     * @return
+     * @throws SQLException
+     */
     public Member save(Member member) throws SQLException {
         String sql = "insert into member(member_id, money) values (?, ?)";
 
@@ -42,8 +50,101 @@ public class MemberRepositoryV0 {
     }
 
     /**
-     * 리소스를 정리할 때는 항상 생성의 역순으로!
-     * 리소스 정리는 필수!
+     * 회원 조회
+     * 
+     * @param memberId
+     * @return
+     * @throws SQLException
+     */
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+            if(rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
+        }
+    }
+
+    /**
+     * 회원 수정
+     * 
+     * @param memberId
+     * @param money
+     * @throws SQLException
+     */
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize={}", resultSize);
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    /**
+     * 회원 삭제
+     * 
+     * @param memberId
+     * @return
+     * @throws SQLException
+     */
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    /**
+     * 리소스를 정리할 때는 항상 생성의 역순으로! 리소스 정리는 필수!
+     * 
      * @param con
      * @param stmt
      * @param rs
--- spring-db1/src/test/java/com/example/itemservice/MemberRepositoryTest.java
+++ spring-db1/src/test/java/com/example/itemservice/MemberRepositoryTest.java
@@ -1,21 +1,48 @@
 package com.example.itemservice;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import org.junit.jupiter.api.Test;
 
 import com.example.springdb1.domain.Member;
 import com.example.springdb1.domain.MemberRepositoryV0;
 
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 public class MemberRepositoryTest {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
+        // save
         Member member = new Member("memberV0", 1000);
         repository.save(member);
+
+        // findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        log.info("member == findMember {}", member == findMember); // false
+        log.info("member equals findMember {}", member.equals(findMember)); // true (EqualsAndHashCode)
+        assertThat(findMember).isEqualTo(member); // EqualsAndHashCode
+
+        // update
+        repository.update(member.getMemberId(), 5000);
+        Member updateMember = repository.findById(member.getMemberId());
+        log.info("updateMember={}", updateMember);
+        log.info("member == updateMember {}", member == updateMember); // false
+        log.info("member equals updateMember {}", member.equals(updateMember)); // true (EqualsAndHashCode)
+        assertThat(updateMember).isEqualTo(new Member(member.getMemberId(), 5000)); // EqualsAndHashCode
+
+        // delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId())).isInstanceOf(NoSuchElementException.class);
     }
 
 }
